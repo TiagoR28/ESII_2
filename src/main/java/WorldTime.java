2c7a1d4fe5404503a02ba1e4492ebc2cc9dcf710
TIAGO ROBERTO MARQUES PACHECO <8130404@estg.ipp.pt>
2019-10-15 23:15:07 +0100

    comit 1

---
+++ src/main/java/WorldTime.java
@@ -0,0 +1,23 @@
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalTime;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WorldTime {
+    public static void main(String[] args) {
+        /**
+         * MUDANÇA DE TIMEZONE
+         */
+        DateTime currentTime = new DateTime(DateTimeZone.forID("Europe/Lisbon"));
+        System.out.println("Portugal: " + currentTime.getHourOfDay() + ":" + currentTime.getMinuteOfHour());
+        DateTime amsterdam = currentTime.withZone(DateTimeZone.forID("Europe/Amsterdam"));
+        System.out.println("Amsterdam: " + amsterdam.getHourOfDay() + ":" + amsterdam.getMinuteOfHour());
+
+    }
+
+
+}
+
+
